9c1cda76fdb52f00fccffcc7fd31fceba83ae053
Анастасия Жукова <levayadev@outlook.com>
2022-05-17 21:30:35 +0300

    Добавьте файлы проекта.

---
+++ Zhukova_PR20/Zhukova_PR20/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Zhukova_PR20.ViewModels;
+
+namespace Zhukova_PR20.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Zhukova_PR20/Zhukova_PR20/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Zhukova_PR20.Models;
+using Zhukova_PR20.ViewModels;
+
+namespace Zhukova_PR20.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
